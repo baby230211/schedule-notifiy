e1004a2a9ea62c5cf8ee465fe7c62f55600192ce
SeanHsieh <t4612310@gmail.com>
2023-04-27 16:51:07 +0800

    update message

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import os
 
 CHANNEL_TOKEN = os.getenv("CHANNEL_TOKEN")
 GROUP_ID = os.getenv("GROUP_ID")
+CUSTOM_MESSAGE = os.getenv("CUSTOM_MESSAGE")
 
 line_bot_api = LineBotApi(CHANNEL_TOKEN)
 
 try:
-    line_bot_api.push_message(GROUP_ID, TextSendMessage(text="Hello World!"))
+    line_bot_api.push_message(GROUP_ID, TextSendMessage(text=CUSTOM_MESSAGE))
 except LineBotApiError as e:
     # error handle
     print(e)
